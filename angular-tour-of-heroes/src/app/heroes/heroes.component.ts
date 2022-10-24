9a24d32f9663c438440198a732a83f84d580bc15
Daniel Paggiola <dpaggiola@gmail.com>
2022-10-24 17:36:54 -0300

    Agregar navegacion en la aplicacion (#6)

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -10,23 +10,13 @@ import {MessageService} from "../message.service";
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
   heroes: Hero[] = [];
-  selectedHero: Hero = {
-    id : 0,
-    name : ''
-  };
 
-  constructor(private heroService: HeroService,
-              private messageService: MessageService) { }
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero) {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
